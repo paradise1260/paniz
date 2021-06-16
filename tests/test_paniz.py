e4fd90ea1bc78b01c2a77ff502f40dabf5c8b5aa
paradise1260 <paniz.fazlali@gmail.com>
2021-06-16 14:34:28 -0700

    adding unit tests

--- tests/test_paniz.py
+++ tests/test_paniz.py
@@ -1,2 +1,14 @@
 from paniz import __version__
 from paniz import paniz
+import pandas as pd
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((paniz.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((paniz.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
